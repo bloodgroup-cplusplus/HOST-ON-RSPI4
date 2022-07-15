e472ddd1701ecbaff7f1242c1008bd65157f3c38
Bhushan <tucater10896601@protonmail.com>
2022-07-15 14:26:53 +0530

    added four files

---
+++ server.js
@@ -0,0 +1,41 @@
+const { readFileSync, writeFileSync } = require ('fs');
+
+const express = require ('express');
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+	const count = readFileSync('./count.txt','utf-8');
+
+	const newCount = parseInt(count) +1
+
+    console.log('count = ',count) ;
+	writeFileSync("./count.txt",newCount);
+	res.send(`
+
+		<!DOCTYPE html>
+		<html lang = "en">
+		<head>
+			<meta charset = "utf-8" />
+			<meta name = "viewport" content = "width = device-width, initial-scale=1" />
+			<title> Rpi Hosted website</title>
+		</head>
+		<body>
+			<h1> Welcome to my website </h1>
+			<p> This page has been viewed ${newCount} times!</p>
+		</body>
+
+
+		</html>
+
+	`)
+
+});
+
+
+
+app.listen(5000, () => console.log("http://localhost:5000/"));
+
+
+
